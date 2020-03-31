ade984d0ada1ad1f6f4c3ab6236b2feebf79a05c
Xinyue Xiang <xxiang2@ualberta.ca>
2020-03-30 23:38:13 -0600

    add the start button for driver to start the ride

---
+++ app/src/main/java/com/example/ontime/InProcessActivity.java
@@ -0,0 +1,14 @@
+package com.example.ontime;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class InProcessActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.in_progress);
+    }
+}
