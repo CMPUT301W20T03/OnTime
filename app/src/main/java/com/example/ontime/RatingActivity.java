1da11f79d5e3a7a694a869cc030ee3b34eb0565c
Fluctray <xudong9@ualberta.ca>
2020-04-01 15:31:45 -0600

    modified rating class

--- app/src/main/java/com/example/ontime/RatingActivity.java
+++ app/src/main/java/com/example/ontime/RatingActivity.java
@@ -4,26 +4,33 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RatingActivity extends AppCompatActivity {
 
     Button good_comment_txt;
     Button bad_comment_txt;
-    String driver_name;
-    Double rating_mark;
+    String driverName;
+    String userName;
+    String rating_mark;
     FirebaseFirestore db;
-
+    String TAG="0";
     public SharedPreferences sharedPreferences;
 
     @Override
@@ -35,41 +42,139 @@ public class RatingActivity extends AppCompatActivity {
         bad_comment_txt = findViewById(R.id.bad_comment_txt);
 
 
-        SharedPreferences sp = getSharedPreferences("current_request", Context.MODE_PRIVATE);
-        driver_name= sp.getString("driver_name","");
+
 
 
         good_comment_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final CollectionReference collectionReference = db.collection("Drivers");
-                final DocumentReference user = db.collection("Drivers").document(driver_name);
-                user.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        rating_mark = documentSnapshot.getDouble("rating");
-                        rating_mark+=1;
-                    }
-                });
+                SharedPreferences sp = getSharedPreferences("current_request", Context.MODE_PRIVATE);
+                String rider_name= sp.getString("rider","");
+                System.out.println(rider_name);
+
+
+                db = FirebaseFirestore.getInstance();
+                final DocumentReference user = db.collection("Requests").document(rider_name);
+                user.get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                if (documentSnapshot.exists()){
+                                    driverName = documentSnapshot.getString("driver");
+                                    System.out.printf(driverName);
+
+                                    db = FirebaseFirestore.getInstance();
+                                    final DocumentReference user1 = db.collection("Drivers").document(driverName);
+                                    user1.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                            rating_mark = documentSnapshot.getString("rating");
+                                            int mark=Integer.parseInt(rating_mark);
+                                            mark+=1;
+                                            rating_mark=Integer.toString(mark);
+                                            final CollectionReference collectionReference = db.collection("Drivers");
+                                            Map<String, Object> data = new HashMap<>();
+
+                                            data.put("rating", rating_mark);
+                                            collectionReference
+                                                    .document(driverName)
+                                                    .update(data)
+                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                        @Override
+                                                        public void onSuccess(Void aVoid) {
+                                                            Log.d(TAG, "Data modification successful");
+                                                            // after accepting the request ...
+
+                                                        }
+                                                    })
+                                                    .addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+                                                            Log.d(TAG, "Data modification failed" + e.toString());
+                                                        }
+                                                    });
+                                        }
+                                    });
+
+
+                                }else{
+                                    Toast.makeText(RatingActivity.this,"user not exist",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+
                 Toast.makeText(RatingActivity.this, "Rating Success!!!!!", Toast.LENGTH_LONG).show();
                 Intent intent=new Intent(RatingActivity.this,WalletActivity.class);
                 startActivity(intent);
-
             }
         });
+
+
+
+
 
         bad_comment_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final CollectionReference collectionReference = db.collection("Drivers");
-                final DocumentReference user = db.collection("Drivers").document(driver_name);
-                user.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        rating_mark = documentSnapshot.getDouble("rating");
-                        rating_mark-=1;
-                    }
-                });
+                SharedPreferences sp = getSharedPreferences("current_request", Context.MODE_PRIVATE);
+                String rider_name= sp.getString("rider","");
+                System.out.println(rider_name);
+
+
+                db = FirebaseFirestore.getInstance();
+                final DocumentReference user = db.collection("Requests").document(rider_name);
+                user.get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                if (documentSnapshot.exists()){
+                                    driverName = documentSnapshot.getString("driver");
+                                    System.out.printf(driverName);
+
+                                    db = FirebaseFirestore.getInstance();
+                                    final DocumentReference user1 = db.collection("Drivers").document(driverName);
+                                    user1.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                            rating_mark = documentSnapshot.getString("rating");
+                                            int mark=Integer.parseInt(rating_mark);
+                                            mark-=1;
+                                            rating_mark=Integer.toString(mark);
+                                            final CollectionReference collectionReference = db.collection("Drivers");
+                                            Map<String, Object> data = new HashMap<>();
+
+                                            data.put("rating", rating_mark);
+                                            collectionReference
+                                                    .document(driverName)
+                                                    .update(data)
+                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                        @Override
+                                                        public void onSuccess(Void aVoid) {
+                                                            Log.d(TAG, "Data modification successful");
+                                                            // after accepting the request ...
+
+                                                        }
+                                                    })
+                                                    .addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+                                                            Log.d(TAG, "Data modification failed" + e.toString());
+                                                        }
+                                                    });
+                                        }
+                                    });
+
+
+                                }else{
+                                    Toast.makeText(RatingActivity.this,"user not exist",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+
                 Toast.makeText(RatingActivity.this, "Rating Success!!!!!", Toast.LENGTH_LONG).show();
                 Intent intent=new Intent(RatingActivity.this,WalletActivity.class);
                 startActivity(intent);
@@ -78,5 +183,5 @@ public class RatingActivity extends AppCompatActivity {
 
 
 
-}
+    }
 }
